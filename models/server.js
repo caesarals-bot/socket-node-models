1304c48ec8ff64d1e3210599ba02b87d13a2c879
cesar londoño <slac_@hotmail.com>
2022-06-19 20:02:09 -0400

    first commit

---
+++ models/server.js
@@ -0,0 +1,45 @@
+// servidor de express
+const express = require('express');
+//Servidor de sockets
+const http = require('http');
+const socketio = require('socket.io')
+const path = require('path');
+const Sockets = require('./sockets');
+
+class Server {
+    constructor(){
+        
+        this.app = express()
+        this.port = process.env.PORT
+        //http server
+        this.server = http.createServer(this.app)
+        // Configuracion del socket 
+        this.io = socketio(this.server)
+
+    }
+    middlewares(){
+        //desplegar el directorio publico
+        this.app.use(express.static( path.resolve(__dirname, '../public') ))
+    }
+
+    configurarSockets(){
+        new Sockets( this.io )
+    }
+
+    execute(){
+        //iniciar middlewares
+        this.middlewares()
+
+        // iniciar sockets
+        this.configurarSockets()
+
+        this.server.listen(this.port, () => {
+            console.log('Server corriendo en el puerto:', this.port)
+        });
+    }
+
+
+
+}
+
+module.exports = Server
---
+++ models/sockets.js
@@ -0,0 +1,25 @@
+
+class Sockets {
+
+    constructor(io){
+
+        this.io = io
+        this.socketEvents()
+
+    }
+
+    socketEvents(){
+        //On connection
+        this.io.on('connection', ( socket ) => { 
+            //Escuchando un evento
+            socket.on('mensaje-to-server', (data) => {
+                console.log(data)
+                this.io.emit('mensaje-from-server', data)
+            })
+        });
+    }
+
+}
+
+
+module.exports = Sockets
